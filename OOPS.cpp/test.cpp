c7f53f357cf04ba2eeeb4ea20a4805981f399214
bhavesh9480 <bhavesh101suthar@gmail.com>
2024-12-11 21:40:36 +0530

    Factorial Using recursion

--- OOPS.cpp/test.cpp
+++ OOPS.cpp/test.cpp
@@ -63,7 +63,7 @@ int main() {
     return 0;
 }*/
 
-#include <iostream>
+/*#include <iostream>
 #include <string>
 
 using namespace std;
@@ -92,6 +92,34 @@ int main() {
 
     cout << "Student 1: " << student1.name << ", " << student1.age << endl;
     cout << "Student 2: " << student2.name << ", " << student2.age << endl;
+
+    return 0;
+}*/
+
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a non-negative integer: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        int result = factorial(num);
+        cout << "Factorial of " << num << " is " << result << endl;
+    }
 
     return 0;
 }
