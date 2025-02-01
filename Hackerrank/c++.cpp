bc11e021a17e9a1765205a54e9dd40d721435651
Bhavesh9480 <bhavesh101suthar@gmail.com>
2025-02-01 12:23:04 +0530

    Functions

--- Hackerrank/c++.cpp
+++ Hackerrank/c++.cpp
@@ -1,4 +1,5 @@
-#include <cmath>
+//Input Output
+/*#include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
@@ -7,9 +8,46 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    //Enter your code here. Read input from STDIN. Print output to STDOUT 
     int a,b,c;
     cin>>a>>b>>c;
     cout<<a+b+c;  
     return 0;
+}*/
+
+//Functions
+/*#include <iostream>
+#include <cstdio>
+using namespace std;
+
+// 
+// Add `int max_of_four(int a, int b, int c, int d)` here.
+// 
+int maximum(int x,int y,int z,int m)
+{
+    if(x>=y && x>=z && x>=m)
+    {
+        return x;
+    }
+    else if(y>=z && y>=m)
+    {
+        return y;
+    }
+    else if(z>=m)
+    {
+        return z;
+    }
+    else {
+    return m;
+    }
 }
+
+int main() {
+    int a, b, c, d,l;
+    cin>>a>>b>>c>>d;
+   l= maximum(a,b,c,d);
+   cout<<l;
+    
+    return 0;
+}*/
+
