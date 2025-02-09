9147f84baf2a1ee0ff2f3132fb332a71523a452c
Bhavesh9480 <bhavesh101suthar@gmail.com>
2025-02-09 21:13:46 +0530

    Integer To roman

--- leetcode/array.cpp
+++ leetcode/array.cpp
@@ -88,7 +88,7 @@ public:
     }
 };*/
 //13. Roman to Integer
-class Solution {
+/*class Solution {
 public:
     int romanToInt(string s) {
         for(char c:s)
@@ -143,5 +143,30 @@ public:
 
             }
             return result;
+    }
+};*/
+
+//12. Integer to roman
+class Solution {
+public:
+    string intToRoman(int num) {
+        unordered_map<int, string> intToRomanMap = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+            {1, "I"}
+        };
+        string s="";
+        vector<int> values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        for(int value:values)
+        {
+            while(num>=value)
+            {
+                s+=intToRomanMap[value];
+                num-=value;
+            }
+        }
+        return s;
+
     }
 };
