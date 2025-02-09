6928aeabc66d19ea904572d2e23fd0ca18c6de68
Bhavesh9480 <bhavesh101suthar@gmail.com>
2025-02-09 21:12:50 +0530

    Roman to integer

--- leetcode/array.cpp
+++ leetcode/array.cpp
@@ -64,7 +64,8 @@ public:
     }
 };*/
 
-class Solution {
+
+/*class Solution {
 public:
     int lengthOfLastWord(string s) {
         // Reverse the string
@@ -84,5 +85,63 @@ public:
         }
 
         return length;
+    }
+};*/
+//13. Roman to Integer
+class Solution {
+public:
+    int romanToInt(string s) {
+        for(char c:s)
+        {
+            if(c=='I'||c=='V'||c=='X'||c=='L'||c=='C'||c=='D'||c=='M')
+            {
+                continue;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+            int result=0;
+            for(int i=0;i<s.length();i++)
+            {
+                switch(s[i]){
+                    case 'I':
+                    result+=1;
+                    break;
+                    case 'V':
+                    result+=5;
+                    break;
+                    case 'X':
+                    result+=10;
+                    break;
+                    case 'L':
+                    result+=50;
+                    break;
+                    case 'C':
+                    result+=100;
+                    break;
+                    case 'D':
+                    result+=500;
+                    break;
+                    case 'M':
+                    result+=1000;
+                    break;
+                }
+                if(i<s.length()-1 && s[i]=='I' &&(s[i+1]=='V'||s[i+1]=='X'))
+                {
+                    result-=2;
+                }
+                if(i<s.length()-1 && s[i]=='X' &&(s[i+1]=='C'||s[i+1]=='L'))
+                {
+                    result-=20;
+                }
+                if(i<s.length()-1 && s[i]=='C' &&(s[i+1]=='D'||s[i+1]=='M'))
+                {
+                    result-=200;
+                }
+
+            }
+            return result;
     }
 };
